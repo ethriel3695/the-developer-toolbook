82e567939ec46e692fb57e1a87beec84e487117e
ethriel3695 <ethriel3695@gmail.com>
2019-04-25 18:08:39 -0600

    Edited travis.yml file to push to aws

--- source.js
+++ source.js
@@ -2,5 +2,6 @@ const execSync = require('child_process').execSync;
 
 const arg = process.argv[2] || 'commit message';
 
+execSync('npm run build');
 execSync('npm run add-commit ' + "\"" + arg + "\"", {stdio:[0, 1, 2]});
 execSync('npm run push', {stdio:[0, 1, 2]});
